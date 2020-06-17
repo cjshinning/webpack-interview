c8b3c5e91b5e313335a573d55ed6444eb9be8dba
cjshinningg <784319417@qq.com>
2020-06-17 18:32:49 +0800

    运行环境

---
+++ src/b.js
@@ -0,0 +1,14 @@
+function fn(){
+    console.log('fn');
+}
+
+const name = 'b';
+const obj = {
+    name: 'lisi'
+}
+
+export default {
+    fn,
+    name,
+    obj
+}
---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import {fn, name, obj} from './a';
+// import {fn, name, obj} from './b';
+import b from './b';
+
+b.fn();
+console.log(b.name);
+console.log(b.obj);
+
+// fn();
+// console.log(name);
+// console.log(obj);
+
+import xxx from './c';
+console.log(xxx);
+
+console.log(100);
+
+
+
+
+// const sum = (a, b) => {
+//     return a + b;
+// }
+// const res = sum(10, 20);
+// console.log(res);
